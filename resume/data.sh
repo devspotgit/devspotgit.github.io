812cfb35e834a4402aa618253bdb8b6a0566d80b
Patrick <jpatricktadie@gmail.com>
2024-09-21 15:23:14 -0400

    some changes

---
+++ resume/data.sh
@@ -0,0 +1,83 @@
+
+
+
+declare -a skills
+skills+=("Computer Netwotking")
+skills+=("Analytical Skills")
+skills+=("Linux/Unix")
+skills+=("C++")
+skills+=("Java")
+skills+=("Team Work")
+skills+=("Circuit Design")
+Skills+=("Simulation Tools")
+Skylls+=("Python")
+
+
+declare -a educations
+educations+=("computer_science")
+educations+=("electrical_engineering")
+
+
+declare -a experiences
+experiences+=("nokia_internship")
+experiences+=("cpos_user_support")
+
+
+declare -A nokia_internship
+nokia_internship[company_name]="Nokia"
+nokia_internship[job_title]="Internship"
+nokia_internship[start_date]="01/2020"
+nokia_internship[end_date]="04/2020"
+nokia_internship[job_details]="nokia_internship_job_details"
+
+declare -a nokia_internship_job_details
+nokia_internship_job_details+=("Created and managed a cloud environment on Openstack platform")
+nokia_internship_job_details+=("Learned Kubernetes and Docker on cloud environment, on linux based system")
+nokia_internship_job_details+=("Tested software component and reported any unusual behaviour")
+nokia_internship_job_details+=("Created Gitlab project to collaborate with my teamCreated Gitlab project to collaborate with my team")
+nokia_internship_job_details+=("Wrote documentation about software specifications and how to use it")
+nokia_internship_job_details+=("Wrote monthly report as a feedback of my advancement to the manager")
+
+
+declare -A cpos_user_support
+cpos_user_support[company_name]="CPOS"
+cpos_user_support[job_title]="User Support"
+cpos_user_support[start_date]="02/2023"
+cpos_user_support[end_date]="Current"
+cpos_user_support[job_details]="cpos_user_support_job_details"
+
+
+declare -a cpos_user_support_job_details
+cpos_user_support_job_details+=("Handled phone call conversation with customers")
+cpos_user_support_job_details+=("Managed tickets through Freshdesk")
+cpos_user_support_job_details+=("Assisted customers on setting up and troubleshooting devices")
+cpos_user_support_job_details+=("Collaborated with third party service provider to provide further assistance to the customers")
+cpos_user_support_job_details+=("Updated data base with customer informations")
+cpos_user_support_job_details+=("Analyzed and identifyied the root causes of technical issues reported by users")
+cpos_user_support_job_details+=("Collaborated with vendors for hardware repair or replacement")
+cpos_user_support_job_details+=("Participated on weekly meetings")
+
+
+
+
+declare -A computer_science
+computer_science[school_name]="University of Ottawa"
+computer_science[program]="Bachelor degree in Computing Technology"
+computer_science[start_date]="09/2016"
+computer_science[end_date]="05/2023"
+
+
+declare -A electrical_engineering
+electrical_engineering[school_name]="University of Ottawa"
+electrical_engineering[program]="Bachelor degree in Electrical engineering"
+electrical_engineering[start_date]="09/2016"
+electrical_engineering[school_name]="05/2023"
+
+
+
+
+
+
+
+
+
